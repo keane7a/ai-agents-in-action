01bf5bcc4ef3fab96f769c7b5aa3fb8da931e12d
keane <keane7a@gmail.com>
2026-07-02 16:55:25 +0100

    abstract setup openai using gemini model

--- chapter_02/01_first_agent.py
+++ chapter_02/01_first_agent.py
@@ -1,21 +1,7 @@
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 from dotenv import load_dotenv
 import os
-
-# Initialise. 
-load_dotenv()
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-
-external_client = AsyncOpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
-
-model = OpenAIChatCompletionsModel(
-    model="gemini-3.1-flash-lite",
-    openai_client=external_client
-)
-
+from setup_openai import model
 
 # Building the agent. 
 instructions = """
