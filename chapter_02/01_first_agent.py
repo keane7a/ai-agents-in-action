2355192f15b3b555cd81a9645e8852c5c918ca7b
keane <keane7a@gmail.com>
2026-07-02 16:42:51 +0100

    add a minimal agent

---
+++ chapter_02/01_first_agent.py
@@ -0,0 +1,44 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+from dotenv import load_dotenv
+import os
+
+# Initialise. 
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-3.1-flash-lite",
+    openai_client=external_client
+)
+
+
+# Building the agent. 
+instructions = """
+You are a research planning assistant.
+
+**TASK INSTRUCTIONS**
+- You will be given a research topic.
+- Your task is to provide a plan on how to research this topic.
+- Output 5 concise tasks (5 words or less) to your plan.
+"""
+
+agent = Agent(
+    name="Research Planner", 
+    instructions=instructions, 
+    model=model
+)
+
+input = "learn about AI agents"
+
+result = Runner.run_sync(
+    agent, 
+    input
+)
+
+print(result.final_output)
