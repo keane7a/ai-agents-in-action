f7037f8dec6a96259955f82e8db60965e431cd62
keane <keane7a@gmail.com>
2026-07-03 13:33:50 +0100

    using agents with local mcp server

---
+++ chapter_03/02_mcp_agent_stdio_server.py
@@ -0,0 +1,35 @@
+import asyncio
+from pathlib import Path 
+from agents import Agent, Runner
+from agents.mcp import MCPServerStdio, MCPServerStdioParams
+from setup_openai import model
+
+SCRIPT = Path(__file__).with_name("01_claude_mcp_server.py").resolve()
+
+async def main(): 
+    async with MCPServerStdio(
+        name="Research Tools",
+        params=MCPServerStdioParams(
+            command="mcp",
+            args=["run", str(SCRIPT)],
+        )
+    ) as research_server: 
+        agent = Agent(
+            model=model,
+            name="Assistant", 
+            instructions="Use the research tools to peform research", 
+            mcp_servers=[research_server]
+        )
+        
+        print("Running: Get the available research sources")
+        
+        result = await Runner.run(
+            agent, 
+            "Get the available research sources"
+        )
+        
+        print(result.final_output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
