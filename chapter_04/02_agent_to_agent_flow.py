bd634145b4f669530121586a7baa33378bb1e142
keane <keane7a@gmail.com>
2026-07-08 12:21:14 +0100

    implement sequential agents

---
+++ chapter_04/02_agent_to_agent_flow.py
@@ -0,0 +1,88 @@
+import asyncio
+import os
+from pathlib import Path
+
+from agents import Agent, Runner
+from agents.mcp import MCPServerStdio, MCPServerStdioParams
+from setup_openai import model
+
+SANDBOX = os.path.dirname(os.path.abspath(__file__))
+SCRIPT = Path(__file__).with_name("01_research_tools_mcp_server.py").resolve()
+
+
+async def main(): 
+    # Instantiate agents 
+    research_agent = Agent(
+        model=model, 
+        name="Research Agent", 
+        instructions="""
+        You are a research assistant. 
+        Your role is to find research sources. 
+        """,         
+    )
+    
+    thinking_agent = Agent(
+        model=model, 
+        name="Thinking Agent", 
+        instructions="""
+        You are a research assistant. 
+        Your role is to plan the research. 
+        """,
+    ) 
+    
+    filesystem_agent = Agent(
+        model=model,
+        name="Filesystem Agent",
+        instructions="""
+        You are a research assistant.
+        Your role is to write the research plan as a text file.
+        """
+    )
+    
+    servers = [
+        MCPServerStdio(
+            name="Research Tools",
+            params=MCPServerStdioParams(
+                command="mcp",
+                args=["run", str(SCRIPT)],
+            ),
+        ),
+        MCPServerStdio(
+            name="sequential-thinking",
+            params={
+                "command": "npx",
+                "args": ["-y", "@modelcontextprotocol/server-sequential-thinking"],
+            },
+        ),
+        MCPServerStdio(
+            name="filesystem",
+            params={
+                "command": "npx",
+                "args": ["-y", "@modelcontextprotocol/server-filesystem", SANDBOX],
+            },
+        ),
+    ]
+    
+    async with (
+        servers[0] as research_srv,
+        servers[1] as thinking_srv,
+        servers[2] as fs_srv,
+    ): 
+        goal = """
+        Produce a research plan to find the book 'The Hitchhiker's Guide to the Galaxy'.
+        """
+        
+        print("Running...", goal)
+        research_agent.mcp_servers = [research_srv]
+        result = await Runner.run(research_agent, goal)
+        thinking_agent.mcp_servers = [thinking_srv]
+        result = await Runner.run(thinking_agent, result.final_output)
+        filesystem_agent.mcp_servers = [fs_srv]
+        result = await Runner.run(filesystem_agent, result.final_output)
+        print(result.final_output)
+        
+
+if __name__ == "__main__":
+    asyncio.run(main())
+        
+        
