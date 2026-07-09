98697107cf10ee1f0ec1f1d79e48fb8ea5bf6a2a
keane <keane7a@gmail.com>
2026-07-09 14:42:57 +0100

    implement tree of thought agent

---
+++ chapter_05/setup_openai.py
@@ -0,0 +1,19 @@
+from agents import AsyncOpenAI, OpenAIChatCompletionsModel
+from dotenv import load_dotenv
+import os
+
+# Initialise OPENAI to use Gemini API
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-3.1-flash-lite",
+    openai_client=external_client
+)
+
+
