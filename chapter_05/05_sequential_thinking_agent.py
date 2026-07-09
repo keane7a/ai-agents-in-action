e902e3c30206c3d7d965075e403370abf598fdee
keane <keane7a@gmail.com>
2026-07-09 15:18:11 +0100

    implement sequential thinking MCP agent.

---
+++ chapter_05/05_sequential_thinking_agent.py
@@ -0,0 +1,37 @@
+import asyncio 
+from agents import Agent, Runner 
+from agents.mcp import MCPServerStdio
+from setup_openai import model
+
+async def main(): 
+    # Initi server
+    thinking_srv = MCPServerStdio(
+        name="sequential-thinking", 
+        params={
+            "command": "npx", 
+            "args": ["-y", "@modelcontextprotocol/server-sequential-thinking"]
+        }
+    )
+    
+    instructions = """You are a helpful planning assistant"""
+    
+    agent = Agent(
+        model=model, 
+        name="Assistant", 
+        instructions=instructions,
+        mcp_servers=[thinking_srv]
+    )
+    
+    async with thinking_srv:
+        tools = await thinking_srv.list_tools()
+        print("Available tools:", tools)
+        
+        goal = """Discover and output the tool and functions you have available"""
+        
+        print("Running...", goal)
+        res = await Runner.run(agent, goal)
+        print(res.final_output)
+        
+
+if __name__ == "__main__":
+    asyncio.run(main())
